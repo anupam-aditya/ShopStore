2aeac3f9a3f6f8ed3005059d1ce55fcf80678d9f
anupam-aditya <anupamaditya99@gmail.com>
2023-11-28 15:53:08 +0530

    orders styling carts

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,33 +4,33 @@ import { GlobalContext } from "../../context/GlobalState";
 import "./Cart.css";
 
 function Cart() {
-  const { cart } = useContext(GlobalContext);
+	const { cart } = useContext(GlobalContext);
 
-  return (
-    <div className="cart-container">
-      <h1>Cart</h1>
-      {!cart.length ? (
-        <p>No Item Added! Please add something to your cart</p>
-      ) : (
-        <>
-          <div className="cart-list">
-            {cart.map((item) => (
-              <div className="cart-item" key={item.id}>
-                <div className="item-price">${item.price}</div>
-                <div className="item-name">{item.name}</div>
-                <div className="item-expectedDelivery">
-                  (Expected Delivery 3 - 6 days)
-                </div>
-              </div>
-            ))}
-          </div>
-          <Link to="/checkout">
-            <button className="item-btn">Next</button>
-          </Link>
-        </>
-      )}
-    </div>
-  );
+	return (
+		<div className="cart-container">
+			<h1>Cart</h1>
+			{!cart.length ? (
+				<p>No Item Added! Please add something to your cart</p>
+			) : (
+				<>
+					<div className="cart-list">
+						{cart.map((item) => (
+							<div className="cart-item" key={item.id}>
+								<div className="item-price">₹{item.price}</div>
+								<div className="item-name">{item.name}</div>
+								<div className="item-expectedDelivery">
+									(Expected Delivery 3 - 6 days)
+								</div>
+							</div>
+						))}
+					</div>
+					<Link to="/checkout">
+						<button className="item-btn">Next</button>
+					</Link>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default Cart;
